151c0402d8008f0fadc7eb2dc5368b822b0c295c
Nicholas Lucien <nicklucien@gmail.com>
2022-05-21 21:46:13 -0700

    Message Board

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,9 @@
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView, 
+    DeleteView
+)
 from django.urls import reverse_lazy
 from .models import Post
 
@@ -11,3 +16,18 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     template_name = "posts/detail.html"
     model = Post
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ['title', 'text']
+
+class PostUpdateView(UpdateView):
+    template_name = "post/edit.html"
+    model = Post
+    fields = ['title', 'text']
+
+class PostDeleteView(DeleteView):
+    template_name = "post/delete.html"
+    model = Post
+    success_url = reverse_lazy('')
