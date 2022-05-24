27e352fb11d8130efdaaf29dd586a59378cbed1a
Brett Bryant <bbryant572@gmail.com>
2022-05-24 17:51:04 -0600

    made migrations

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n^axhex=ywpnwo=yf=k0dbrs7w$dq0353*=+%c&=b$j7dh_827'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['fsdi-ch25-final-project.herokuapp.com', '127.0.0.1']
 
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
