6dbc6b54343d70efc74e145c6e57aa627657a3aa
Brett Bryant <bbryant572@gmail.com>
2022-06-02 19:56:20 -0600

    added some changes. Not finished

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
 
-from .views import SignUpView
+from .views import SignUpView, RegisterView, profile
 from .views import PasswordChangeView, PasswordChangeSuccessView
 
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', profile, name='profile'),
     path('passwordchange/<int:pk>',
          PasswordChangeView.as_view(), name='password_change'),
     path('passwordchange/success/',
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeDoneView
 from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import RegisterForm
 
 
 class SignUpView(CreateView):
@@ -16,3 +20,31 @@ class PasswordChangeView(PasswordChangeView):
 
 class PasswordChangeSuccessView(PasswordChangeDoneView):
     template_name = 'registration/password_change_done.html'
+
+
+class RegisterView(CreateView):
+    form_class = RegisterForm
+    initial = {'key': 'value'}
+    template_name = 'registration/register.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}')
+
+            return redirect(to='/')
+
+        return render(request, self.template_name, {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'registration/profile.html')
