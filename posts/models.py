608fd8329ed8751a7d6c6a0d07fc4080045e4382
Brett Bryant <bbryant572@gmail.com>
2022-06-12 11:00:05 -0600

    made changes in class

--- posts/models.py
+++ posts/models.py
@@ -2,9 +2,22 @@ from django.db import models
 from django.urls import reverse
 
 
+class Image(models.Model):
+    image = models.ImageField(upload_to='media/img')
+    caption = models.CharField(max_length=128, blank=True)
+    title = models.CharField(max_length=255, unique=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+
 class Content(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField()
+    content = models.CharField(max_length=5000, null=True)
+    photo = models.ForeignKey(
+        Image, related_name="image_img", on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.title
@@ -13,11 +26,11 @@ class Content(models.Model):
         return reverse('content_detail', args=[self.id])
 
 
-class Image(models.Model):
-    Photography = models.ForeignKey(
-        Content, related_name="image_img", on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='media/img')
-    title = models.CharField(max_length=255, unique=True)
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Content, related_name="comments", on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    body = models.TextField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return '%s - %s' % (self.post.title, self.name)
