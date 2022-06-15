4d3220a38422998fa4f09a8690e42c0d869921cd
Jasmine Green-Key <greinkejs1069@marybaldwin.edu>
2022-06-14 21:47:47 -0500

    New Account Page / Home CSS

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
 
 
-from .views import SignUpView
+from .views import SignUpView, AccountUpdateView, AccountCreateView
 from .views import PasswordChangeView, PasswordChangeSuccessView
 
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('passwordchange/<int:pk>',
+     path('signup/', SignUpView.as_view(), name='signup'),
+     path('passwordchange/<int:pk>',
          PasswordChangeView.as_view(), name='password_change'),
-    path('passwordchange/success/',
+     path('passwordchange/success/',
          PasswordChangeSuccessView.as_view(), name='password_success'),
+     path('userprofileupdate/<int:pk>', AccountUpdateView.as_view(), name='user_profile_update'),
+     path('userprofile/<int:pk>', AccountCreateView.as_view(), name='user_profile')
 ]
